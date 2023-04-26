4c084b77e6f991d3534920bb240d67cf318714d5
Gautam-pawar <gautam.pawar.23.2@mountblue.tech>
2023-04-26 14:54:33 +0530

    Added StoreAdvertise component

---
+++ src/Components/StoreAdvertise/StoreAdvertise.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+function StoreAdvertise() {
+  return (
+    <div className="w-50">
+      <img src="application-advertise.png" alt="addvertsie" />
+    </div>
+  );
+}
+export default StoreAdvertise;
