da6f2e966cc035161e5a69107381ebdd628992b8
Gautam-pawar <gautam.pawar.23.2@mountblue.tech>
2023-04-26 14:56:19 +0530

    Updated App.js according newly added Componenets

--- src/App.js
+++ src/App.js
@@ -1,9 +1,30 @@
 import "./App.css";
-
+import Navbar from "./Components/Navbar/Navbar";
+import Hero from "./Components/Hero/Hero";
+import StoriesTab from "./Components/StoriesTab/StoriesTab";
+import EventsSearch from "./Components/EventsSearch/EventsSearch";
+import About from "./Components/About/About";
+import UpcommingEvents from "./Components/UpcommingEvents/UpcommingEvents";
+import PopularGroups from "./Components/PopularGroups/PopularGroups";
+import StoreAdvertise from "./Components/StoreAdvertise/StoreAdvertise";
+import MeetupStories from "./Components/MeetupStories/MeetupStories";
+import Footer from "./Components/Footer/Footer";
 function App() {
-  return <div className="App">
-    <h1>Meetup</h1>
-  </div>;
+  return (
+    <div className="App">
+      <Navbar />
+      <Hero />
+      <StoriesTab />
+      <EventsSearch />
+      <About />
+      <button className="joinMeetupButton">join Meetup</button>
+      <UpcommingEvents />
+      <PopularGroups />
+      <StoreAdvertise />
+      <MeetupStories />
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
