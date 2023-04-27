4656b3631137c89f2a9de4c7c30986bd088f9b65
Gautam-pawar <gautam.pawar.23.2@mountblue.tech>
2023-04-27 14:44:38 +0530

    Added Home component for future homepage wrapping

---
+++ src/Components/Home/Home.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+// import EventsSearch from "./Components/EventsSearch/EventsSearch";
+// import About from "./Components/About/About";
+// import UpcommingEvents from "./Components/UpcommingEvents/UpcommingEvents";
+// import PopularGroups from "./Components/PopularGroups/PopularGroups";
+// import StoreAdvertise from "./Components/StoreAdvertise/StoreAdvertise";
+// import MeetupStories from "./Components/MeetupStories/MeetupStories";
+// import Hero from "./Components/Hero/Hero";
+// import StoriesTab from "./Components/StoriesTab/StoriesTab";
+
+export default function Home() {
+  return (
+    <div>
+      {/* <Hero />
+      <StoriesTab />
+      <EventsSearch />
+      <About />
+      <UpcommingEvents />
+      <PopularGroups />
+      <StoreAdvertise />
+      <MeetupStories /> */}
+    </div>
+  );
+}
