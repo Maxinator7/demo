bf048e640b5a1bfcb99e4824d915389c72cbdc79
Gautam-pawar <gautam.pawar.23.2@mountblue.tech>
2023-04-27 14:41:59 +0530

    Updated EventSearch component for seacrhing events

--- src/Components/EventsSearch/EventsSearch.js
+++ src/Components/EventsSearch/EventsSearch.js
@@ -1,11 +1,25 @@
 import React from "react";
 import "./EventsSearch.css";
+import { Link } from "react-router-dom";
+import { handleSearchEvent } from "../../Reducer/redux";
+import { useDispatch, useSelector } from "react-redux";
+
 
 export default function EventsSearch() {
+  const {searchedEvent,SearchedLocation}= useSelector(store => store.MeetupData)
+  const dispatch=useDispatch()
+  const handleChange = (e) => {
+    const {name,value}=e.target
+    dispatch(handleSearchEvent({name:name, value: value}))
+    // console.log(name);
+    // console.log(value);
+  };
   return (
     <div className="searchBar">
       <div className="eventSearch w-75 justify-content-center ">
-        <h3 style={{ color: "black" , fontWeight :"bolder" }}>What do you want to do?</h3>
+        <h3 style={{ color: "black", fontWeight: "bolder" }}>
+          What do you want to do?
+        </h3>
         <div className="d-flex gap-4 ">
           <div className="input-group mb-3 w-50">
             <span className="input-group-text" id="inputGroup-sizing-default">
@@ -15,6 +29,9 @@ export default function EventsSearch() {
               ></i>
             </span>
             <input
+            name="searchedEvent"
+            value={searchedEvent}
+              onChange={handleChange}
               type="text"
               className="form-control"
               aria-label="Sizing example input"
@@ -39,7 +56,9 @@ export default function EventsSearch() {
             />
           </div>
         </div>
-        <button className="searchButton">Search</button>
+        <Link to="/searchresults" style={{ textDecoration: "none" }}>
+          <button className="searchButton">Search</button>
+        </Link>
       </div>
     </div>
   );
