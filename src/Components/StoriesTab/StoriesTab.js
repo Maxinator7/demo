bb8559456acb2bd8d6de3a10bba7455cb1a995af
Gautam-pawar <gautam.pawar.23.2@mountblue.tech>
2023-04-27 10:47:24 +0530

    Added data to the story cards in StoriesTab component and updated its styling

--- src/Components/StoriesTab/StoriesTab.js
+++ src/Components/StoriesTab/StoriesTab.js
@@ -1,11 +1,28 @@
 import React from "react";
-import Storycard from "../StoryCard/Storycard";
+import { useSelector } from "react-redux";
+import "./StoriesTab.css";
 
 export default function StoriesTab() {
-  let Stories = new Array(3).fill(0);
+  const { events } = useSelector((store) => store.MeetupData);
 
-  const allStories = Stories.map((arr) => {
-    return <Storycard />;
+  const allStories = events.slice(0, 3).map((data) => {
+    return (
+      <div className="card ms-4 mt-4">
+        <div>
+          <img className="story-Img" src={data.image} alt="story img" />
+        </div>
+        <div className="ms-4 mt-2">
+          {" "}
+          <h5 style={{ color: "blue" }}>
+            {data.location}{" "}
+            <i
+              className="fa-solid fa-arrow-right"
+              style={{ color: "#2461cc" }}
+            ></i>
+          </h5>
+        </div>
+      </div>
+    );
   });
   return (
     <div className="d-flex justify-content-center mt-4 mb-4">{allStories}</div>
