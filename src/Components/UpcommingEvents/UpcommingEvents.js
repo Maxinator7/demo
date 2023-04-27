d890d22bd34c8d9067acf787c57c08f293086f44
Gautam-pawar <gautam.pawar.23.2@mountblue.tech>
2023-04-27 10:43:41 +0530

    updated UpcommingEvents component and its styling

--- src/Components/UpcommingEvents/UpcommingEvents.js
+++ src/Components/UpcommingEvents/UpcommingEvents.js
@@ -1,23 +1,35 @@
 import React from "react";
-import Storycard from "../StoryCard/Storycard";
 import "./UpcommingEvents.css";
-
-let events = new Array(5).fill(0);
-
-let upcommingEvents = events.map((data) => {
-  return <Storycard />;
-});
+import { useSelector } from "react-redux";
 
 function UpcommingEvents() {
+  const { events } = useSelector((store) => store.MeetupData);
+
+  let upcommingEvents = events.map((arr) => {
+    return (
+      <div className="upcommingEvents">
+        <img className="story-Img" src={arr.image} alt="story img" />
+        <div className="card-body">
+          <h5 className="card-title">
+            {arr.date}
+            {arr.time}
+          </h5>
+          <h5 className="card-title">{arr.title}</h5>
+          <h5 className="card-title">Hosted by {arr.personName}</h5>
+        </div>
+      </div>
+    );
+  });
+
   return (
-    <div className="upcommingEvents">
+    <div className="Main">
       <div className="topText">
         {" "}
         <h3 className="font-weight-bold">Upcoming online events</h3>
-        <p>Explore more Events</p>
+        <p style={{color:"blue"}}>Explore more Events</p>
       </div>
 
-      <div className="d-flex">{upcommingEvents}</div>
+      <div className="allEvents">{upcommingEvents}</div>
     </div>
   );
 }
