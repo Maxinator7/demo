9829105fc246a78b042fd1bf0a89077c7782213b
Gautam-pawar <gautam.pawar.23.2@mountblue.tech>
2023-04-27 14:41:17 +0530

    Updated Reducerfiles

--- src/Reducer/redux.js
+++ src/Reducer/redux.js
@@ -7,12 +7,27 @@ const initialState = {
   events: data,
   Groups: groups,
   meetupStories: stories,
+  // EmailAddress : "",
+  // Password: "",
+  searchedEvent: "",
+  SearchedLocation: "",
 };
 
 const dataSlice = createSlice({
   name: "MeetupData",
   initialState,
-  reducers: {},
-});
+  reducers: {
+    handleSearchEvent: (state, { payload }) => {
+      console.log(payload);
+      state[payload.name] = payload.value;
+    },
 
+    handleSearchLocation: (state, { payload }) => {
+      // console.log(action);
+      console.log(payload);
+      // action.searchedEvent=
+    },
+  },
+});
+export const { handleSearchEvent, handleSearchLocation } = dataSlice.actions;
 export default dataSlice.reducer;
