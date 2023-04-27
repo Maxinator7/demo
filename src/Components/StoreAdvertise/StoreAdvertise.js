cd72d99ad6b823238d4ceabe7b50fba182bc49e8
Gautam-pawar <gautam.pawar.23.2@mountblue.tech>
2023-04-27 10:49:05 +0530

    updated advertise image by converting the screenshot to jpg

--- src/Components/StoreAdvertise/StoreAdvertise.js
+++ src/Components/StoreAdvertise/StoreAdvertise.js
@@ -2,8 +2,8 @@ import React from "react";
 
 function StoreAdvertise() {
   return (
-    <div className="w-50">
-      <img src="application-advertise.png" alt="addvertsie" />
+    <div className="w-50 mt-5 mb-5">
+      <img src="meetupAdd.jpg" alt="addvertsie" />
     </div>
   );
 }
