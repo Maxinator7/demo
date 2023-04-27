cd160929557483988c297f148b83a2fa6ae6d8b8
Gautam-pawar <gautam.pawar.23.2@mountblue.tech>
2023-04-27 10:42:09 +0530

    updated EventsSearch components styling

--- src/Components/EventsSearch/EventsSearch.js
+++ src/Components/EventsSearch/EventsSearch.js
@@ -5,34 +5,34 @@ export default function EventsSearch() {
   return (
     <div className="searchBar">
       <div className="eventSearch w-75 justify-content-center ">
-        <h1 style={{color : "black"}}>What do you want to do?</h1>
-        <div className="d-flex ">
-          <div class="input-group mb-3 w-50">
-            <span class="input-group-text" id="inputGroup-sizing-default">
+        <h3 style={{ color: "black" , fontWeight :"bolder" }}>What do you want to do?</h3>
+        <div className="d-flex gap-4 ">
+          <div className="input-group mb-3 w-50">
+            <span className="input-group-text" id="inputGroup-sizing-default">
               <i
-                class="fa-solid fa-magnifying-glass "
+                className="fa-solid fa-magnifying-glass "
                 style={{ color: "#afb5c0" }}
               ></i>
             </span>
             <input
               type="text"
-              class="form-control"
+              className="form-control"
               aria-label="Sizing example input"
               aria-describedby="inputGroup-sizing-default"
               placeholder="Search for tennis or UX design "
             />
           </div>
-          <div class="input-group mb-3 w-50 ">
-            <span class="input-group-text" id="inputGroup-sizing-default">
+          <div className="input-group mb-3 w-50 ">
+            <span className="input-group-text" id="inputGroup-sizing-default">
               {" "}
               <i
-                class="fa-sharp fa-solid fa-location-dot"
+                className="fa-sharp fa-solid fa-location-dot"
                 style={{ color: "#c1c7d2" }}
               ></i>
             </span>
             <input
               type="text"
-              class="form-control"
+              className="form-control"
               aria-label="Sizing example input"
               aria-describedby="inputGroup-sizing-default"
               placeholder="Neighbourhood or City or Zip"
