6aa8a008cc6f2208baf4642fbf9ea2c77e44d695
Gautam-pawar <gautam.pawar.23.2@mountblue.tech>
2023-05-02 01:41:56 +0530

    formatted code

--- src/Reducer/store.js
+++ src/Reducer/store.js
@@ -1,7 +1,7 @@
-import { configureStore } from '@reduxjs/toolkit'
-import dataReducer from './redux'
-export const store =configureStore({
-    reducer:{
-        MeetupData:dataReducer
-    }
-})
+import { configureStore } from "@reduxjs/toolkit";
+import dataReducer from "./redux";
+export const store = configureStore({
+  reducer: {
+    MeetupData: dataReducer,
+  },
+});
