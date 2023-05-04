6a5754e4550895f7f2b918d2dbb000ec34a17abc
Gautam-pawar <gautam.pawar.23.2@mountblue.tech>
2023-05-05 04:44:11 +0530

    updated Upcomming Events styling

--- src/Components/UpcommingEvents/UpcommingEvents.js
+++ src/Components/UpcommingEvents/UpcommingEvents.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "./UpcommingEvents.css";
 import { useSelector } from "react-redux";
+import { Link } from "react-router-dom";
 
 function UpcommingEvents() {
   const { events } = useSelector((store) => store.MeetupData);
@@ -8,15 +9,17 @@ function UpcommingEvents() {
   let upcommingEvents = events.map((arr) => {
     return (
       <div className="upcommingEvents">
-        <img className="story-Img" src={arr.image} alt="story img" />
-        <div className="card-body">
-          <h5 className="card-title">
-            {arr.date}
-            {arr.time}
-          </h5>
-          <h5 className="card-title">{arr.title}</h5>
-          <h5 className="card-title">Hosted by {arr.personName}</h5>
-        </div>
+        <Link to={"/EventDetails"}>
+          <img className="story-Img" src={arr.image} alt="story img" />
+          <div className="card-body">
+            <h5 className="card-title">
+              {arr.date}
+              {arr.time}
+            </h5>
+            <h5 className="card-title">{arr.title}</h5>
+            <h5 className="card-title">Hosted by {arr.personName}</h5>
+          </div>
+        </Link>
       </div>
     );
   });
@@ -26,7 +29,7 @@ function UpcommingEvents() {
       <div className="topText">
         {" "}
         <h3 className="font-weight-bold">Upcoming online events</h3>
-        <p style={{color:"blue"}}>Explore more Events</p>
+        <p style={{ color: "blue" }}>Explore more Events</p>
       </div>
 
       <div className="allEvents">{upcommingEvents}</div>
