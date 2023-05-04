67f0da5859bdc9c9483815eaf80b7cee2a63b210
Gautam-pawar <gautam.pawar.23.2@mountblue.tech>
2023-05-04 09:31:58 +0530

    Updated Navbar component routes

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "./Navbar.css";
-
+import { Link } from "react-router-dom";
 export default function Navbar() {
   return (
     <div>
@@ -34,6 +34,8 @@ export default function Navbar() {
           </div>
         </div>
       </nav>
+
+      {/* ======= signup=================== */}
       <div
         class="modal fade"
         id="staticBackdrop"
@@ -79,17 +81,23 @@ export default function Navbar() {
                 ></i>
                 Continue with Apple
               </button>
-              <button
-                className="btn"
-                data-toggle="modal"
-                data-target="#signupModal"
-              >
-                <i
-                  className="fa-solid fa-envelope"
-                  style={{ color: "#6e7072" }}
-                ></i>
-                Sign up with email
-              </button>
+              <Link to={"/signup"} style={{textDecorationLine:"none"}}>
+                <button
+                  className="btn"
+                  data-target="#myModal"
+                  data-toggle="modal"
+                  //data-target="#signupModal"
+                  data-keyboard="false"
+                  data-bs-dismiss="modal"
+                  aria-label="Close"
+                >
+                  <i
+                    className="fa-solid fa-envelope"
+                    style={{ color: "#6e7072" }}
+                  ></i>
+                  Sign up with email
+                </button>
+              </Link>
             </div>
           </div>
         </div>
@@ -112,14 +120,22 @@ export default function Navbar() {
                 aria-label="Close"
               ></button>
             </div>
-            <div class="modal-body">
-              <div className="emailSignup">
+            <div class="modal-body border border-secondary">
+              <div className="loginmodel">
                 <img src="Meetup_Logo1.png" className="signupLogo"></img>
                 <h1>Log in</h1>
-                <p>Not a member yet ? Sign up</p>
+                <p>
+                  Not a member yet ?
+                  <span className="text-primary"> Sign up</span>
+                </p>
                 <form>
-                  <div class="form-group">
-                    <label for="formGroupExampleInput2">Email address</label>
+                  <div class="form-group mt-4">
+                    <label
+                      style={{ fontWeight: "bolder" }}
+                      for="formGroupExampleInput2"
+                    >
+                      Email address
+                    </label>
                     <input
                       type="text"
                       class="form-control"
@@ -127,8 +143,14 @@ export default function Navbar() {
                       placeholder="Enter your email address"
                     />
                   </div>
-                  <div class="form-group">
-                    <label for="formGroupExampleInput">Password</label>
+                  <div class="form-group mt-4">
+                    <label
+                      className="d-flex justify-content-between"
+                      for="formGroupExampleInput"
+                    >
+                      <div style={{ fontWeight: "bolder" }}>Password</div>
+                      <div className="text-primary">Forgot password</div>
+                    </label>
                     <input
                       type="text"
                       class="form-control"
@@ -137,59 +159,83 @@ export default function Navbar() {
                     />
                   </div>
 
-                  <button className="logInBtn">Log in</button>
+                  <div
+                    style={{
+                      display: "flex",
+                      flexDirection: "column",
+                      justifyContent: "center",
+                    }}
+                  >
+                    <button className="logInBtn">Log in</button>
 
-                  <h6>
-                    ----------------------------------- or
-                    -----------------------------------
-                  </h6>
+                    <h6>
+                      ----------------------------------- or
+                      -----------------------------------
+                    </h6>
 
-                  <button className="btn">
-                    <i
-                      className="fa-brands fa-facebook-f"
-                      style={{ color: "#155cd5" }}
-                    ></i>
-                    Continue with facebook
-                  </button>
-                  <button className="btn">
-                    <i
-                      className="fa-brands fa-google"
-                      style={{ color: "#ec1313" }}
-                    ></i>
-                    Continue with Google
-                  </button>
-                  <button className="btn">
-                    <i
-                      className="fa-brands fa-apple"
-                      style={{ color: "#111212" }}
-                    ></i>
-                    Continue with Apple
-                  </button>
+                    <button className="btn">
+                      <i
+                        className="fa-brands fa-facebook-f"
+                        style={{ color: "#155cd5" }}
+                      ></i>
+                      Continue with facebook
+                    </button>
+                    <button className="btn">
+                      <i
+                        className="fa-brands fa-google"
+                        style={{ color: "#ec1313" }}
+                      ></i>
+                      Continue with Google
+                    </button>
+                    <button className="btn">
+                      <i
+                        className="fa-brands fa-apple"
+                        style={{ color: "#111212" }}
+                      ></i>
+                      Continue with Apple
+                    </button>
 
-                  <h6>Issues with log in ?</h6>
+                    <button className="btn">
+                      {" "}
+                      <h6 className="text-primary">Issues with log in ?</h6>
+                    </button>
+                  </div>
                 </form>
               </div>
             </div>
           </div>
         </div>
       </div>
-
-      <div class="modal fade" id="signupModal" tabindex="-1" role="dialog" aria-labelledby="signupModalLabel" aria-hidden="true">
-  <div class="modal-dialog" role="document">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h5 class="modal-title" id="signupModalLabel">Sign Up</h5>
-        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-          <span aria-hidden="true">&times;</span>
-        </button>
+      {/* ==================================Login=========================== */}
+      <div
+        class="modal fade"
+        id="signupModal"
+        tabindex="-1"
+        role="dialog"
+        aria-labelledby="signupModalLabel"
+        aria-hidden="true"
+      >
+        <div class="modal-dialog" role="document">
+          <div class="modal-content">
+            <div class="modal-header">
+              <h5 class="modal-title" id="signupModalLabel">
+                Sign Up
+              </h5>
+              <button
+                type="button"
+                class="close"
+                data-dismiss="modal"
+                aria-label="Close"
+              >
+                <span aria-hidden="true">&times;</span>
+              </button>
+            </div>
+            <div class="modal-body">
+              {/* <!-- Sign Up form goes here --> */}
+            </div>
+          </div>
+        </div>
       </div>
-      <div class="modal-body">
-        {/* <!-- Sign Up form goes here --> */}
-      </div>
-    </div>
-  </div>
-</div>
-
     </div>
   );
 }
