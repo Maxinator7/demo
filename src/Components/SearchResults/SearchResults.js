a36c5789b1f979e607aa7e59ede692ccb66d400c
Gautam-pawar <gautam.pawar.23.2@mountblue.tech>
2023-05-04 09:33:50 +0530

    Updated SearchRresults component

---
+++ src/Components/SearchResults/SearchResults.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+export default function SearchResults() {
+  return (
+    <div
+      style={{
+        height: "15rem",
+        color: "black",
+        display: "flex",
+        justifyContent: "center",
+      }}
+    >
+      <h1> No Search results found !!!</h1>
+    </div>
+  );
+}
