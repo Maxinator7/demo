9de5a2d2cee3928271d9e4edeccbeae1864ef567
Gautam-pawar <gautam.pawar.23.2@mountblue.tech>
2023-05-05 04:42:50 +0530

    updated SearchResults structure and styling

--- src/Components/SearchResults/SearchResults.js
+++ src/Components/SearchResults/SearchResults.js
@@ -1,16 +1,59 @@
 import React from "react";
+import "./SearchResults.css";
+import Events from "../Events/Events";
+import Groups from "../Groups/Groups";
 
-export default function SearchResults() {
+function SearchResults() {
   return (
-    <div
-      style={{
-        height: "15rem",
-        color: "black",
-        display: "flex",
-        justifyContent: "center",
-      }}
-    >
-      <h1> No Search results found !!!</h1>
+    <div className="searchResults">
+      <h1 className="mt-5">Event near Bangalore,IN</h1>
+      <div className="tabs">
+        <p className="d-flex gap-4 ms-4">
+          <a
+            className="button"
+            data-bs-toggle="collapse"
+            href="#collapseExample"
+            role="button"
+            aria-expanded="false"
+            aria-controls="collapseExample"
+          >
+            <h4> Events</h4>
+          </a>
+          {/* <button
+            className="btn "
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#collapseExample"
+            aria-expanded="false"
+            aria-controls="collapseExample"
+          >
+            Button with data-bs-target
+          </button> */}
+          <a
+            className="button "
+            data-bs-toggle="collapse"
+            href="#collapseExample1"
+            role="button"
+            aria-expanded="false"
+            aria-controls="collapseExample"
+          >
+            <h4> Groups</h4>
+          </a>
+        </p>
+        <div className="collapse" id="collapseExample">
+          <div class="card card-body">
+            <Events />
+          </div>
+        </div>
+
+        <div className="collapse" id="collapseExample1">
+          <div className="card card-body">
+            <Groups />
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
+
+export default SearchResults;
